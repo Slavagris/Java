0e6144cea612033462cd09624fa085ba44d42249
Slavagris <83882153+Slavagris@users.noreply.github.com>
2021-11-16 10:39:30 +0300

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,27 @@
+class Employee {
+    String name;
+    String post;
+    String email;
+    String phoneNumber;
+    int salary;
+    int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public Employee(String name, String post, String email, String phoneNumber, int salary, int age) {
+    this.name = name;
+    this.post = post;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.salary = salary;
+    this.age = age;
+    }
+
+    public void print() {
+        System.out.println("Имя: " + name + " | " + " Должность: " + post + " | " + " e-mail: " + email + " | " +
+                " Тел.: " + phoneNumber + " | " + " ЗП: " + salary + " | " + " Возраст: " + age);
+
+    }
+}
